52fb9c9e57a62983d6a90040805ea5fc008847f0
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-06 17:42:03 -0500

    Update Speller.c

--- Week 5 - Data Structures/Speller.c
+++ Week 5 - Data Structures/Speller.c
@@ -121,17 +121,3 @@ bool unload(void)
     }
     return true;
 }
-
-
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    int i = 0;
-    while (i < 3)
-    {
-        printf("meow\n");
-        i++;
-    }
-}
