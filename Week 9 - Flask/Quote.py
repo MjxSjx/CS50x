8b50623d79cb9cdb35a955be52d26b68e7867f5b
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-07 21:48:22 -0500

    Create Quote.py

---
+++ Week 9 - Flask/Quote.py
@@ -0,0 +1,19 @@
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "GET":
+        return render_template("quote.html")
+
+    else:
+        symbol = request.form.get("symbol")
+
+        if not symbol:
+            return apology("Missing symbol", 403)
+        # use helpers.py look fn
+        stock = lookup(symbol.upper())
+
+        if not stock:
+            return apology("Invalid symbol entry", 403)
+        # if info found render
+        return render_template("quoted.html", stock=stock)
