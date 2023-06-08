17128a0cbc937ae9c23eff44c5ff4667e1181f18
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-07 22:30:29 -0500

    Update fun_text.js

--- Week 8 - HTML, CSS, JavaScript/fun_text.js
+++ Week 8 - HTML, CSS, JavaScript/fun_text.js
@@ -1,4 +1,4 @@
-const inputBox = document.getElementById('input-box'); 
+const inputBox = document.getElementById('input-box');  
 const submitBtn = document.getElementById('submit-btn');
  
 submitBtn.addEventListener('click', processInput);
