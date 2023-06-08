3a4aa3781dc75ca982d946814833f1c3a74f9b28
Matt Johnson <111515875+MjxSjx@users.noreply.github.com>
2023-06-07 21:50:01 -0500

    Create History.py

---
+++ Week 9 - Flask/History.py
@@ -0,0 +1,7 @@
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    user_id = session["user_id"]
+    transaction_db = db.execute("SELECT * FROM transactions WHERE user_id = :id", id=user_id)
+    return render_template("history.html",  transaction = transaction_db)
